7e2122af185d8d0198ce2c895195f8454aa63a14
Kacper Rak <rzstunts@gmail.com>
2022-06-08 20:06:48 +0200

    api1

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function main() {
     console.log(humidity,temp,wind,clouds)
     console.log(weatherJson)
 
-    document.getElementById('temperature').innerHTML="temperatura"+temp
+    document.getElementById('temperature').innerHTML="temperatura"+temp+"°C"
     
 }
 
